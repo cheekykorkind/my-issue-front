4b4660a4db47deb543c51a61ffbfe81261d67bd7
cheekykorkind <cheekorkind@gmail.com>
2021-03-22 09:45:51 -0400

    select cypress test추가

--- cypress/integration/first.js
+++ cypress/integration/first.js
@@ -5,7 +5,7 @@ describe('test1', () => {
     // });
     
     it('test1 it', () => {
-      cy.visit(`http://192.168.33.16:3000/`)
-      cy.contains("Welcome to Next.js!", { timeout: 20000 })
+      cy.visit(`http://localhost:3000/`)
+      cy.contains("Hello World", { timeout: 20000 })
     });  
   })
